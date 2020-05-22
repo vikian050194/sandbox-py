e262a21b59d55ba9f645ddbd1ac0535f58d1411f
Kirill Vinogradov <vikian050194@gmail.com>
2020-05-22 09:38:32 +0300

    Add git.py

---
+++ git.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+"""Interactive tool for repositories cloning from GitHub"""
+
+import requests
+import subprocess
+
+def get_repos_metadata(user = "vikian050194"):
+    """Info about get_repos_metadata.
+    
+    Args:
+        user: GitHub user name
+        
+    Returns: 
+        List of repositories metadata
+    """
+    url = f"https://api.github.com/users/{user}/repos"
+    r = requests.get(url = url)
+    return r.json()
+
+
+def do_interactive_clone(repos):
+    """Go through list of repositories and clone some of them according to user response
+    
+    Args:
+        repos: List of repositories metadata
+    """
+    for repo in repos:
+        print("%s: %s"%(repo["name"], repo["language"]))
+        print("Clone this repo? (y/n)") 
+        response = input()
+        
+        if response == "y":
+            bashCommand = ["git", "clone" , repo["clone_url"], repo["name"].lower().replace('-', '/', 1)]
+            process = subprocess.Popen(bashCommand, stdout=subprocess.PIPE)
+            output, error = process.communicate()
+            print(output)
+
+
+def clone_repos():
+    """Entry point"""
+
+    try:
+        repos = get_repos_metadata()
+        reposCount = len(repos)
+        print(f"{reposCount} repositories are discovered")
+        
+        if len(repos) != 0:
+            do_interactive_clone(repos)
+    except Exception as e:
+        print(e)   
+
+
+if __name__ == "__main__":
+    clone_repos()
--- script.py
+++ script.py
@@ -2,53 +2,16 @@
 
 """Module docstring"""
 
-import requests
-import subprocess
+def generate_file(min = 0, max = 10):
+    output = "output.txt"
 
-def get_repos_metadata(user = "vikian050194"):
-    """Info about get_repos_metadata.
-    
-    Args:
-        nothing
-        
-    Returns: 
-        List of repos metadata.
-    """
-    url = f"https://api.github.com/users/{user}/repos"
-    r = requests.get(url = url)
-    return r.json()
+    lines = []
 
+    for x in range(min, max + 1):
+        lines.append(f"{x}\n")
 
-def do_interactive_clone(repos):
-    """Info about do_interactive_clone.
-    
-    Args:
-        repos: List of repos metadata.
-    """
-    for repo in repos:
-        print("%s: %s"%(repo["name"], repo["language"]))
-        print("Clone this repo? (y/n)") 
-        response = input()
-        
-        if response == "y":
-            bashCommand = ["git", "clone" , repo["clone_url"], repo["name"].lower()]
-            process = subprocess.Popen(bashCommand, stdout=subprocess.PIPE)
-            output, error = process.communicate()
-            print(output)
-
-
-def clone_repos():
-    try:
-        repos = get_repos_metadata()
-        reposCount = len(repos)
-        print(f"{reposCount} repositories are discovered")
-        
-        if len(repos) != 0:
-            do_interactive_clone(repos)
-    except Exception as e:
-        print(e)   
-
+    with open(output, 'w') as out_file:
+        out_file.writelines(lines)
 
 if __name__ == "__main__":
-    clone_repos()#comment1
-    #comment2
+    generate_file()
