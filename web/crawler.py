29567f81000ccec35aaf8032e787c3e7c8002984
Kirill Vinogradov <vikian050194@gmail.com>
2021-10-09 10:08:32 +0300

    temp commit

--- web/crawler.py
+++ web/crawler.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-"""Crawler"""
-
 import os
 import requests
 from html.parser import HTMLParser
@@ -9,31 +7,83 @@ from html.parser import HTMLParser
 from download import download as download_file
 from load import load as load_page_content
 
-site = "https://lingua.com"
-page = "/french/reading/"
 
-class HrefParser(HTMLParser):
+site = "https://notificationsounds.com"
+page = "/notification-sounds/pages/"
+pages = dict()
+
+
+class FooParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
         for attr in attrs:
             (name, value) = attr
-            if name == "href" and value.endswith(".pdf"):
-                file = value[5:] 
-                print(file)
-                download_file( + file)
+            if name == "class" and value == "block text-2xl text-link mb-6 leading-6":
+                initial_page = attrs[0][1]
+                _, sound_type, sound_name = initial_page.split("/")
+                if sound_type in pages:
+                    pages_list = pages.get(sound_type)
+                    pages_list.append(sound_name)
+                else:
+                    pages_list = [sound_name]
+                    pages[sound_type] = pages_list
+                continue
 
 
-def crawl(url):
+files = set()
+
+
+class BarParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        for attr in attrs:
+            (name, value) = attr
+            if name == "href" and value.endswith(".mp3"):
+                file = value
+                values = value.split("/")
+                folder = values[3]
+                name = values[-1]
+                if not name in files:
+                    files.add(name)
+                    print(file)
+                    download_file(file, folder)
+                break
+
+
+def foo_crawl(url):
     try:
         page = load_page_content(url)
+        page = page.decode("utf-8")
 
         # fo = open("page.html", "r+")
         # page = fo.read()
         # fo.close()
-        
-        parser = HrefParser()
+
+        parser = FooParser()
         parser.feed(page)
     except Exception as e:
         print(e)
 
-if __name__ == "__main__":
-    crawl(site + page)
+
+def bar_crawl(url):
+    try:
+        page = load_page_content(url)
+        page = page.decode("utf-8")
+
+        # fo = open("page.html", "r+")
+        # page = fo.read()
+        # fo.close()
+
+        parser = BarParser()
+        parser.feed(page)
+    except Exception as e:
+        print(e)
+
+
+for i in range(1, 49):
+    url = site + page + str(i)
+    foo_crawl(url)
+
+for sound_type, pages_list in pages.items():
+    pages_list = sorted(pages_list)
+    for page_item in pages_list:
+        url = site + "/" + sound_type + "/" + page_item
+        bar_crawl(url)
--- web/download.py
+++ web/download.py
@@ -1,7 +1,35 @@
-def download(url):
+import os
+import requests
+
+def download(url, folder):
     try:
+        if not os.path.exists(folder):
+            os.makedirs(folder)
         filename = url.split("/")[-1]
-        r = requests.get(url, allow_redirects=True)
-        open(filename, "wb").write(r.content)
+        filename = folder + "/" + filename
+        headers = {
+            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            "accept-encoding": "gzip, deflate, br",
+            "accept-language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7,cs-CZ;q=0.6,cs;q=0.5",
+            "cache-control": "no-cache",
+            "cookie": "_ga=GA1.2.28811514.1621103113; _gid=GA1.2.1407532990.1621103113",
+            "pragma": "no-cache",
+            "referer": "https://notificationsounds.com/",
+            "sec-ch-ua": '" Not A;Brand";v="99", "Chromium";v="90", "Google Chrome";v="90"',
+            "sec-ch-ua-mobile": "?0",
+            "sec-fetch-dest": "document",
+            "sec-fetch-mode": "navigate",
+            "sec-fetch-site": "same-site",
+            "sec-fetch-user": "?1",
+            "upgrade-insecure-requests": "1",
+            "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"
+        }
+        # headers={}
+        r = requests.get(url, allow_redirects=False, headers=headers)
+        if r.status_code == 200:
+            open(filename, "wb").write(r.content)
+            print(folder, url.split("/")[-1])
+        else:
+            print(r)
     except Exception as e:
         print(e)
