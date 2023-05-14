760869c48142b650e6bafbef490273d574523d8c
Kirill Vinogradov <vikian050194@gmail.com>
2023-05-14 10:09:55 +0200

    update crawler.py

--- web/crawler.py
+++ web/crawler.py
@@ -9,23 +9,30 @@ from html.parser import HTMLParser
 from download import download as download_file
 from load import load as load_page_content
 
-site = "https://lingua.com"
-page = "/french/reading/"
+site = "https://hpaudiobooks.club"
+page = "/harry-potter-and-prisoner-azkaban/"
+
+index = 1
 
 class HrefParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
-        for attr in attrs:
-            (name, value) = attr
-            if name == "href" and value.endswith(".pdf"):
-                file = value[5:] 
-                print(file)
-                download_file( + file)
+        if tag == "source":
+            for attr in attrs:
+                (name, value) = attr
+                try:
+                    if name == "src" and value.endswith(f".mp3?_={index}"):
+                        file = value[5:] 
+                        print(file)
+                        download_file( + file)
+                        index += 1
+                except:
+                    pass
 
 
 def crawl(url):
     try:
         page = load_page_content(url)
-
+        page = page.decode()
         # fo = open("page.html", "r+")
         # page = fo.read()
         # fo.close()
